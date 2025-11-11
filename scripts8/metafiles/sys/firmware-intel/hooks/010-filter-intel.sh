fb3a1c35c709e1294bcae1b9ddbcf6df9d342f58
fcanatta <canatafernando26@gmail.com>
2025-11-11 18:06:33 -0300

    Create 010-filter-intel.sh

---
+++ scripts8/metafiles/sys/firmware-intel/hooks/010-filter-intel.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+: "${DESTDIR:=/}"
+src="."
+dst="${DESTDIR}/usr/lib/firmware"
+
+mkdir -p "$dst"
+
+for f in $(find "$src" -type f \( -name "iwlwifi*" -o -name "ibt*" \)); do
+    d="$(dirname "$f")"
+    mkdir -p "$dst/$d"
+    cp -f "$f" "$dst/$d/"
+done
+
+echo "[firmware-intel] Instalado"
