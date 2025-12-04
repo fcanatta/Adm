8e93b34b35effece19cd483a9bbfe8ea70cbc27b
fcanatta <canatafernando26@gmail.com>
2025-12-04 14:49:02 -0300

    Create grep.sh

---
+++ scripts19/package/base/grep/grep.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# grep-3.12.sh
+
+PKG_VERSION="3.12"
+SRC_URL="https://ftp.gnu.org/gnu/grep/grep-${PKG_VERSION}.tar.xz"
+SRC_MD5=""
+
+pkg_build() {
+    : "${NUMJOBS:=1}"
+
+    rm -rf build
+    mkdir -v build
+    cd build
+
+    ../configure \
+        --prefix=/usr
+
+    make -j"${NUMJOBS}"
+    make DESTDIR="${DESTDIR}" install
+}
