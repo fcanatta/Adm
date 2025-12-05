95c5e98f7dad443cd811d2fe1dc8b74cfdeeb205
fcanatta <canatafernando26@gmail.com>
2025-12-05 11:56:23 -0300

    Update musl.sh

--- scripts20/packages/base/musl/musl.sh
+++ scripts20/packages/base/musl/musl.sh
@@ -38,6 +38,18 @@
 
 set -euo pipefail
 
+# Definir quais libcs esse pacote suporta:
+#   - para gcc final: glibc, musl, uclibc-ng
+#   - para glibc: apenas glibc
+#   - para musl: apenas musl
+REQUIRED_LIBCS="glibc musl uclibc-ng"
+
+# Carregar validador de profile
+source /usr/src/adm/lib/adm_profile_validate.sh
+
+# Validar profile atual
+adm_profile_validate
+
 MUSL_VERSION="1.2.5"
 MUSL_NAME="musl-${MUSL_VERSION}"
 
